7c0aa8e1777b20f23eef95d13c3925dabe1743c5
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2025-06-23 23:04:28 +0530

    register touchup

--- healthhacked/frontend/src/pages/Register.tsx
+++ healthhacked/frontend/src/pages/Register.tsx
@@ -220,7 +220,7 @@ export function Register() {
                 type="submit"
                 disabled={isLoading}
                 variant='primary'
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none disabled:opacity-50"
               >
                 {isLoading ? 'Creating account...' : 'Create account'}
               </Button>
